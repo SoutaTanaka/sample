57b833e4162ac67d6b3421faa01fe7269b33b638
SoutaTanaka <tanakakesouta@gmail.com>
2017-03-28 16:35:05 +0900

    Initial Commit

---
+++ AiSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AiSwiftTest
+//
+//  Created by 田中颯太 on 2017/03/28.
+//  Copyright © 2017年 田中颯太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AiSwiftTestTests/AiSwiftTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  AiSwiftTestTests.swift
+//  AiSwiftTestTests
+//
+//  Created by 田中颯太 on 2017/03/28.
+//  Copyright © 2017年 田中颯太. All rights reserved.
+//
+
+import XCTest
+@testable import AiSwiftTest
+
+class AiSwiftTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
